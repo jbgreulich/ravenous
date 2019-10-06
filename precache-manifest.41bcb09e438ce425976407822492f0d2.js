00ec71a21cfc06474c9ef23d48a3a2d7651d1921
jbgreulich <jessica@greulich.me>
2019-10-06 18:57:53 -0400

    Updates

---
+++ precache-manifest.41bcb09e438ce425976407822492f0d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46a53128bfe2fa2c0795ad63c31a5bb9",
+    "url": "/ravenous/index.html"
+  },
+  {
+    "revision": "bb4dc2d8efb83316c18f",
+    "url": "/ravenous/static/css/main.5d23b1fa.chunk.css"
+  },
+  {
+    "revision": "2790c44532533aa1f119",
+    "url": "/ravenous/static/js/2.2668f2aa.chunk.js"
+  },
+  {
+    "revision": "bb4dc2d8efb83316c18f",
+    "url": "/ravenous/static/js/main.829f03df.chunk.js"
+  },
+  {
+    "revision": "1444c97d4f0ed3607f27",
+    "url": "/ravenous/static/js/runtime~main.f8ce170b.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
